3cf9b1f6f920a189c58de79fbaa0a8d4a2894d80
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2013-06-07 23:57:25 +0200

    Confuguration refactoring

--- inc/kernel_cfg.h
+++ inc/kernel_cfg.h
@@ -64,14 +64,14 @@
 # define CFG_SCHED_TIME_QUANTUM         10U
 #endif
 
-/**@brief       Scheduler system timer mode
+/**@brief       System timer mode
  * @details     Possible values are:
  *              - 0U - fixed mode
  *              - 1U - inhibited mode
  *              - 2U - adaptive mode
  */
-#if !defined(CFG_SCHED_SYSTMR_MODE)
-# define CFG_SCHED_SYSTMR_MODE          1U
+#if !defined(CFG_SYSTMR_MODE)
+# define CFG_SYSTMR_MODE                1U
 #endif
 
 /**@brief       The frequency of system tick event
